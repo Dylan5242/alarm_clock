21429a4be63fab5435d489f606b30adfe0c85fa7
Dmitry <test@mail.ru>
2025-05-03 10:36:25 +0300

    new time

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,7 +19,7 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_plus_clicked()
 {
     QListWidgetItem *item = new QListWidgetItem(ui->listWidget);
-    ListElementWidget *widget = new ListElementWidget("Понедельник", "12:00", ui->listWidget);
+    ListElementWidget *widget = new ListElementWidget("Понедельник", "13:00", ui->listWidget);
     item->setSizeHint(widget->sizeHint());
     ui->listWidget->addItem(item);
     ui->listWidget->setItemWidget(item, widget);
